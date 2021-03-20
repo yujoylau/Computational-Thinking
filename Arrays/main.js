e4b202dcee30cd22ec564924c6c73f21d6975321
yujoylau <76860599+yujoylau@users.noreply.github.com>
2021-03-20 18:01:42 +0800

    Update main.js

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,1 +1,6 @@
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let elementAtIndexFive = numArray[5];
+
+// This will print the value of variable elementAtIndexFive to the console
+console.log('The array element with index 5 is: ', elementAtIndexFive);
 
