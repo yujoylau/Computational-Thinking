a8ff0efcd5c84607345472182e9e53e624ab5630
yujoylau <76860599+yujoylau@users.noreply.github.com>
2021-03-20 18:11:53 +0800

    Add files via upload

---
+++ Variables/variables02.js
@@ -0,0 +1,12 @@
+//Instructions
+// - Check the instructions in the summary file and calculate the current year
+
+let currentYear = 2020;
+//console log the result
+console.log('current year result: ', currentYear); //should be 2020
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = currentYear;
+}
+
+
