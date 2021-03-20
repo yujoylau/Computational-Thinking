92afe91bc6001698cc9d3e996c76597ddf1b9f36
yujoylau <76860599+yujoylau@users.noreply.github.com>
2021-03-20 18:06:29 +0800

    Create main.js

---
+++ Objects/main.js
@@ -0,0 +1,26 @@
+/*
+Instructions
+Given the function getProduct, 
+- Return an object with the following {key: value} properties
+- id:productId
+- serialNumber: 'WD579000'
+- manufacturer: 'Apple'
+- price: 1500
+*/
+
+function getProduct(productId) {
+  //write your code here
+  return {
+    id: productId,
+    serialNumber: 'WD579000',
+    manufacturer:'Apple',
+    price: 1500
+    }
+}
+//open your browser console to check the results
+console.log('result: ' + JSON.stringify(getProduct(1)));
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = getProduct;
+}
